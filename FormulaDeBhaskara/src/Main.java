e904a3b0c7139d38d9bafe2bc00239ead4f53b52
Nathalia Lima <110511358+nathalia-lima@users.noreply.github.com>
2022-10-19 21:16:26 -0300

    Arquivos Java Fórmula de Bháskara

---
+++ FormulaDeBhaskara/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double A = input.nextDouble();
+        double B = input.nextDouble();
+        double C = input.nextDouble();
+
+        if ((A == 0) | (B * B - 4 * A * C < 0)) {
+            System.out.println("Impossivel calcular");
+            return;
+        }
+
+        double bascara = Math.sqrt((B * B) - 4 * A * C);
+        double R1 = (-B + bascara) / (2 * A);
+        double R2 = (-B - bascara) / (2 * A);
+        System.out.println(String.format("R1 = %.5f", R1));
+        System.out.println(String.format("R2 = %.5f", R2));
+    }
+}
